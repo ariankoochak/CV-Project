5057fc123671107e0fcc4b055a7e974df5f1aee0
Arian <arian.koochak@gmail.com>
2023-07-22 14:55:51 +0330

    fix svgs

--- cv-project/src/components/SkillsScreen/SkillScreen.jsx
+++ cv-project/src/components/SkillsScreen/SkillScreen.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import CppIcon from "../Svgs/CppIcon/CppIcon";
 import JqueryIcon from "../Svgs/JqueryIcon/JqueryIcon";
+import NodeJsIcon from "../Svgs/NodeJsIcon/NodeJsIcon";
+import PythonIcon from "../Svgs/PythonIcon/PythonIcon";
+import ReactIcon from "../Svgs/ReactIcon/ReactIcon";
+import SqlIcon from "../Svgs/SqlIcon/SqlIcon";
+import SwiftIcon from "../Svgs/SwiftIcon/SwiftIcon";
 export default function SkillScreen() {
     return (
         <div className="SkillsScreen">
@@ -8,6 +13,11 @@ export default function SkillScreen() {
             <div>
                 <CppIcon width = '50'/>
                 <JqueryIcon width = '50' height = '50'/>
+                <NodeJsIcon width='50' height= '50'/>
+                <PythonIcon width = '50' height = '50'/>
+                <ReactIcon width = '50' height= '50'/>
+                <SqlIcon width = '50' height ='50'/>
+                <SwiftIcon width = '50' height='50'/>
             </div>
         </div>
     );
